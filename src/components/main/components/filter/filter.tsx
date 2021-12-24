ed828a5c6eb13c757874f67084b88cc3a1a6bae8
Azarak <abbatd@gmail.com>
2021-12-24 12:42:47 +1000

    Добавил фильтрацию по цене

--- src/components/main/components/filter/filter.tsx
+++ src/components/main/components/filter/filter.tsx
@@ -1,45 +1,97 @@
-import { useState } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useDidUpdateEffect } from '../../../../hooks/use-did-update-effect';
 import { useDidMountEffect } from '../../../../hooks/use-did-mount-effect';
+import { createAPI } from '../../../../services/api';
 import { filterGuitarsAction } from '../../../../store/api-actioms';
 import { FilterQuery } from '../../../../const';
 import { GuitarFilterType } from '../../../../types/filter';
+import { Guitar } from '../../../../types/guitar';
+
+const MIN_PRICE = 0;
+
+const api = createAPI();
+const getInfo = async () => {
+  const { data } = await api.get<Guitar[]>('/guitars?_sort=price&_order=asc');
+  return data;
+};
 
 export default function Filter(): JSX.Element {
   const history = useHistory();
   const dispatch = useDispatch();
 
   const [locationSearch] = useState(history.location.search);
+
+  const [placeholder, setPlaceholder] = useState({
+    min: MIN_PRICE,
+    max: MIN_PRICE,
+  });
+  const [price, setPrice] = useState({
+    min: '',
+    max: '',
+    query: '',
+  });
   const [guitarType, setGuitarType] = useState<GuitarFilterType>(
     {
-      acoustic: false,
-      electric: false,
-      ukulele: false,
+      acoustic: locationSearch.includes(FilterQuery.Acoustic),
+      electric: locationSearch.includes(FilterQuery.Electric),
+      ukulele: locationSearch.includes(FilterQuery.Ukulele),
     },
   );
 
   useDidMountEffect(() => {
-    setGuitarType({
-      acoustic: locationSearch.includes(FilterQuery.Acoustic),
-      electric: locationSearch.includes(FilterQuery.Electric),
-      ukulele: locationSearch.includes(FilterQuery.Ukulele),
+    getInfo().then((value) => {
+      setPlaceholder({
+        min: value[0].price,
+        max: value[value.length - 1].price,
+      });
     });
   });
 
-  useDidUpdateEffect(() => {
+  useEffect(() => {
     const query = `${
       guitarType.acoustic ? FilterQuery.Acoustic : ''
     }${
       guitarType.electric ? FilterQuery.Electric : ''
     }${
       guitarType.ukulele ? FilterQuery.Ukulele : ''
-    }`;
+    }${price.query}`;
 
     dispatch(filterGuitarsAction(query));
     history.push(`?${query}`);
-  }, [dispatch, guitarType.acoustic, guitarType.electric, guitarType.ukulele]);
+  }, [dispatch, guitarType.acoustic, guitarType.electric, guitarType.ukulele, history, price.query]);
+
+  const onPriceMinBlur = () => {
+    let min = Math.trunc(Math.abs(+price.min));
+    const max = Math.trunc(Math.abs(+price.max));
+
+    if (min < placeholder.min) {
+      min = placeholder.min;
+    }
+
+    if (price.min !== '' && (max > min)) {
+      setPrice({min: `${min}`, max: `${max}`, query: `&price_gte=${min}&price_lte=${max}`});
+      return;
+    }
+    setPrice({...price, min: `${min}`});
+  };
+
+  const onPriceMaxBlur = () => {
+    const min = Math.trunc(Math.abs(+price.min));
+    let max = Math.trunc(Math.abs(+price.max));
+
+    if (max > placeholder.max) {
+      max = placeholder.max;
+    }
+
+    if (price.min !== '' && (max > min)) {
+      setPrice({min: `${min}`, max: `${max}`, query: `&price_gte=${min}&price_lte=${max}`});
+      return;
+    }
+    setPrice({...price, max: `${max}`});
+  };
 
   return (
     <form className="catalog-filter">
@@ -49,11 +101,29 @@ export default function Filter(): JSX.Element {
         <div className="catalog-filter__price-range">
           <div className="form-input">
             <label className="visually-hidden">Минимальная цена</label>
-            <input type="number" placeholder="1 000" id="priceMin" name="от"/>
+            <input
+              onInput={(evt) => setPrice({...price, min: evt.currentTarget.value})}
+              type="number"
+              placeholder={`${placeholder.min}`}
+              id="priceMin"
+              name="от"
+              value={price.min}
+              // onBlur={(evt) => onPriceBlur(evt.target.id, price.min)}
+              onBlur={onPriceMinBlur}
+            />
           </div>
           <div className="form-input">
             <label className="visually-hidden">Максимальная цена</label>
-            <input type="number" placeholder="30 000" id="priceMax" name="до"/>
+            <input
+              onInput={(evt) => setPrice({...price, max: evt.currentTarget.value})}
+              type="number"
+              placeholder={`${placeholder.max}`}
+              id="priceMax"
+              name="до"
+              value={price.max}
+              // onBlur={(evt) => onPriceBlur(evt.target.id, price.max)}
+              onBlur={onPriceMaxBlur}
+            />
           </div>
         </div>
       </fieldset>
--- src/hooks/use-did-update-effect.ts
+++ src/hooks/use-did-update-effect.ts
@@ -3,12 +3,12 @@ import {
   useRef
 } from 'react';
 
-export const useDidUpdateEffect = (effect: () => void, deps: unknown) => {
-  const isInitialMount = useRef(false);
+export const useDidUpdateEffect = (effect: () => void, deps: unknown[]) => {
+  const isInitialMount = useRef(true);
 
   useEffect(() => {
     isInitialMount.current
-      ? effect()
-      : isInitialMount.current = true;
+      ? isInitialMount.current = false
+      : effect();
   }, [effect, deps]);
 };
